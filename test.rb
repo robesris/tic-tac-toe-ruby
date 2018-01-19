03cf30620e320d83c610047da3f99f955b80d724
Rob Esris <resris@paperlesspost.com>
2018-01-18 23:24:30 -0500

    Test for bigger board

--- test.rb
+++ test.rb
@@ -48,3 +48,30 @@ end
 # O X X
 # X O O
 # _ O X
+
+puts "\n\n"
+puts "Winning on a larger board"
+g = TicTacToe::Game.new(5)
+g.play 0, 2
+g.play 0, 0
+g.play 0, 1
+g.play 1, 1
+g.play 1, 0
+g.play 1, 2
+g.play 2, 1
+g.play 2, 2
+g.play 3, 2
+g.play 4, 4
+g.play 4, 2
+g.play 3, 3
+if g.winner == "O"
+  puts "PASS"
+else
+  puts "FAILURE: Expected O to Win but game result was #{g.winner}"
+end
+
+# O X X _ _
+# X O O _ _
+# _ X O _ _
+# _ _ X O _
+# _ _ X _ O
